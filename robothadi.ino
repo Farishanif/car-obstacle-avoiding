37db5afc9873230d5c93bc925efab9b2a8f2f5bf
Farishanif <71810570+Farishanif@users.noreply.github.com>
2021-07-27 14:26:16 +0700

    Update robothadi.ino

--- robothadi.ino
+++ robothadi.ino
@@ -1,152 +1,192 @@
-#include <AFMotor.h>
+#include <Servo.h>  
 #include <NewPing.h>
-#include <Servo.h>
+// connect motor controller pins to Arduino digital pins
+// motor one
+int enA = 7;
+int in1 = 6;
+int in2 = 5;
+// motor two
+int enB = 2;
+int in3 = 4;
+int in4 = 3;
+// Turn speed
+int ts = 170;
+// Turn period
+int tp = 100;
+// No turn driving speed, motor A
+int ntA = 155;
+// No turn driving speed, motor B
+int ntB = 155;
 
-#define TRIG_P1 A0
-#define TRIG_P2 A1 
-#define ECHO_P1 A2
-#define ECHO_P2 A3 
-#define MAX_DISTANCE 25
-#define MAX_SPEED 250 // sets speed of DC  motors
 
-boolean goesForward=false;
-int distanceR = 100;
-int distanceL = 100;
-int speedSet = 0;
 
-AF_DCMotor motor1(8, MOTOR12_1KHZ); 
-AF_DCMotor motor2(9, MOTOR12_1KHZ);
+// Sonar1 setup
+// defines pins numbers
+const int trigPin = 10;
+const int echoPin = 11;
+// defines variables
+long duration;
+int distance;
 
-Servo servoL;
-Servo servoR;
+// Sonar2 setup
+// defines pins numbers
+const int trig2Pin = 12;
+const int echo2Pin = 13;
+// defines variables
+long duration2;
+int distance2;
 
-NewPing sonar1(TRIG_P1, ECHO_P1, MAX_DISTANCE); 
-NewPing sonar2(TRIG_P2, ECHO_P2, MAX_DISTANCE); 
+Servo servo_motor1; //our servo name
+Servo servo_motor2; //our servo name
 
 void setup()
 {
-//pinMode(motor1, OUTPUT);
-//pinMode(motor2, OUTPUT);
-//pinMode(TRIG_P1, OUTPUT); 
-//pinMode(ECHO_P1, INPUT);
-//pinMode(TRIG_P2, OUTPUT); 
-//pinMode(ECHO_P2, INPUT);
-//Serial.begin(9600);
-//while (! Serial);
-servoL.attach(11);  
-servoR.attach(12);
-servoL.write(115); 
-servoR.write(115);
-delay(2000);
-distanceL = readPing(sonar1);
-distanceR = readPing(sonar2);
-delay(100);
-distanceL = readPing(sonar1);
-distanceR = readPing(sonar2);
-delay(100);
+// set all the motor control pins to outputs
+pinMode(enA, OUTPUT);
+pinMode(enB, OUTPUT);
+pinMode(in1, OUTPUT);
+pinMode(in2, OUTPUT);
+pinMode(in3, OUTPUT);
+pinMode(in4, OUTPUT);
+pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+pinMode(trig2Pin, OUTPUT); // Sets the trig2Pin as an Output
+pinMode(echo2Pin, INPUT); // Sets the echo2Pin as an Input
+  servo_motor1.attach(8); //our servo pin
+  servo_motor2.attach(9); //our servo pin
+
+Serial.begin(9600); // Starts the serial communication
 }
 
 void loop()
 {
-// int distanceR = 0;
-// int distanceL =  0;
-// delay(40);
- 
- if(distanceL || distanceR <=15)
- {
-  moveStop();
-  delay(100);
-  moveBackward();
-  delay(300);
-  moveStop();
-  delay(200);
-  ////////look side/////////
-  servoL.write(170); 
-  servoR.write(50); 
-  delay(500);
-  distanceL = readPing(sonar1);
-  distanceR = readPing(sonar2);
-  delay(100);
-  servoR.write(115); 
-  servoL.write(115);
-  delay(200);
-  /////////////////////////
-  if(distanceR>=distanceL)
-  {
-    turnRight();
-    moveStop();
-  }else if(distanceR<=distanceL)
-  {
-    turnLeft();
-    moveStop();
-  }
- }
- else
- {
-  moveForward();
- }
- 
-distanceL = readPing(sonar1);
-distanceR = readPing(sonar2);
-delay(100);
+// Find distance Sonar….
+// Clears the trigPin
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+// Sets the trigPin on HIGH state for 10 micro seconds
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+// Reads the echoPin, returns the sound wave travel time in microseconds
+duration = pulseIn(echoPin, HIGH);
+// Calculating the distance
+distance= duration*0.034/2;
+// Prints the distance on the Serial Monitor
+Serial.print("Distance1: ");
+Serial.println(distance);
+
+// Find distance Sonar2….
+// Clears the trigPin
+digitalWrite(trig2Pin, LOW);
+delayMicroseconds(2);
+// Sets the trigPin on HIGH state for 10 micro seconds
+digitalWrite(trig2Pin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trig2Pin, LOW);
+// Reads the echoPin, returns the sound wave travel time in microseconds
+duration2 = pulseIn(echo2Pin, HIGH);
+// Calculating the distance
+distance2= duration2*0.034/2;
+// Prints the distance on the Serial Monitor
+Serial.print("Distance2: ");
+Serial.println(distance2);
+
+//driving the robot
+if (distance < 20 || distance2 < 20) { 
+  stopMotors(); 
+  servo_motor1.write(0);
+  servo_motor2.write(180);
+  delay (500); 
+  servo_motor1.write(90);
+  servo_motor2.write(90);
+  delay (500); 
+  if (distance < distance2) { 
+    turnRight(); delay (1000); 
+    } if (distance > distance2){ 
+      turnLeft();
+      delay (1000);
 }
 
-int readPing(NewPing sonar) { 
-  delay(70);
-  int cm = sonar.ping_cm();
-  if(cm==0)
-  {
-    cm = 250;
-  }
-  return cm;
+if (distance == distance2)
+{ revers();
+delay (1000);
+}
 }
 
-void moveStop() {
-  motor1.run(RELEASE); 
-  motor2.run(RELEASE);
-  } 
-  
-void moveForward() {
+if (distance > 20) forward();
 
- if(!goesForward)
-  {
-    goesForward=true;
-    motor1.run(FORWARD);      
-    motor2.run(FORWARD);   
-   for (speedSet = 0; speedSet < MAX_SPEED; speedSet +=2) // slowly bring the speed up to avoid loading down the batteries too quickly
-   {
-    motor1.setSpeed(speedSet);
-    motor2.setSpeed(speedSet);
-    delay(5);
-   }
-  }
 }
 
-void moveBackward() {
-    goesForward=false;
-    motor1.run(BACKWARD);      
-    motor2.run(BACKWARD); 
-  for (speedSet = 0; speedSet < MAX_SPEED; speedSet +=2) // slowly bring the speed up to avoid loading down the batteries too quickly
-  {
-    motor1.setSpeed(speedSet);
-    motor2.setSpeed(speedSet);
-    delay(5);
-  }
-}  
-
-void turnRight() {
-  motor1.run(FORWARD);
-  motor2.run(BACKWARD);   
-  delay(500);
-  motor1.run(FORWARD);      
-  motor2.run(FORWARD);      
-} 
- 
-void turnLeft() {
-  motor1.run(BACKWARD);     
-  motor2.run(FORWARD);  
-  
-  delay(500);
-  motor1.run(FORWARD);     
-  motor2.run(FORWARD);
-}  
+void revers()
+{
+// turn on motor A
+digitalWrite(in1, LOW);
+digitalWrite(in2, HIGH);
+// set speed to ntA ***No turn driving speed, motor A***
+analogWrite(enA, ntA);
+// turn on motor B
+digitalWrite(in3, LOW);
+digitalWrite(in4, HIGH);
+// set speed to ntB ***No turn driving speed, motor B***
+analogWrite(enB, ntB);
+// digitalWrite(in4, LOW);
+}
+void forward()
+{
+// turn on motor A
+digitalWrite(in1, HIGH);
+digitalWrite(in2, LOW);
+// set speed to ntA ***No turn driving speed, motor A***
+analogWrite(enA, ntA);
+// turn on motor B
+digitalWrite(in3, HIGH);
+digitalWrite(in4, LOW);
+// set speed to ntB ***No turn driving speed, motor B***
+analogWrite(enB, ntB);
+// digitalWrite(in4, LOW);
+}
+void turnRight()
+{
+// turn on motor A
+digitalWrite(in1, LOW);
+digitalWrite(in2, HIGH);
+// set speed to ts ***Turn Speed***
+analogWrite(enA, ts);
+// turn on motor B
+digitalWrite(in3, HIGH);
+digitalWrite(in4, LOW);
+// set speed to ts ***Turn Speed***
+analogWrite(enB, ts);
+// Turn period
+delay(tp);
+// Stop motors
+digitalWrite(in1, LOW);
+digitalWrite(in4, LOW);
+}
+void turnLeft()
+{
+// turn on motor A
+digitalWrite(in1, HIGH);
+digitalWrite(in2, LOW);
+// set speed to ts ***Turn Speed***
+analogWrite(enA, ts);
+// turn on motor B
+digitalWrite(in3, LOW);
+digitalWrite(in4, HIGH);
+// set speed to ts ***Turn Speed***
+analogWrite(enB, ts);
+//Turn period
+delay(tp);
+// Stop motors
+digitalWrite(in2, LOW);
+digitalWrite(in3, LOW);
+}
+void stopMotors()
+{
+// Stop motors
+digitalWrite(in1, LOW);
+digitalWrite(in2, LOW);
+digitalWrite(in3, LOW);
+digitalWrite(in4, LOW);
+}
